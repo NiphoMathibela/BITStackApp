bfad78715bbffc66f82faa4477ce72d37ca98949
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2023-04-19 21:46:23 +0200

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./front-end/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'orange': '#FEAC56',
+      'green': '#37BD6B',
+      'lightGreen': 'rgba(55, 189, 107, 0.28)',
+      'gray': '#666666',
+      'black': '#171215',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
