8714cd9e17b1c1debe79dc8b309f4d4d2b5aa2d4
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2023-04-21 23:37:36 +0200

    Changes to Nav

--- routes/BottomNav.js
+++ routes/BottomNav.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home'
 import Favourite from '../screens/Favourite'
 import Recipes from '../screens/Recipes'
 import Profile from '../screens/Profile'
-import ExpDate from '../screens/ExpDate'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -25,10 +24,10 @@ function BottomNav() {
                     borderTopWidth: 0,
                     elevation: 0,
                     shadowOpacity: 0,
-                    height: 100,
-                    borderRadius: 10,
+                    height: 78,
+                    borderRadius: 8,
                     marginHorizontal: 20,
-                    marginBottom: 50,
+                    marginBottom: 19,
                     marginTop: 10,
                     position: 'absolute',
                     left: 0,
@@ -37,7 +36,7 @@ function BottomNav() {
                     shadowColor: '#000',
                     shadowOffset: {
                         width: 0,
-                        height: 10,
+                        height: 8,
                     },
                     shadowOpacity: 0.53,
                     shadowRadius: 13.97,
