34e5f3eb8133fe23d7e4ef9e642963d9c1f564a4
Donatelo27 <97600974+DonBos27@users.noreply.github.com>
2023-04-21 09:30:00 +0200

    Added pages,appbar

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import MyStackNav from './routes/MyStackNav';
+
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Lets get this W gents!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStackNav />
+    </NavigationContainer>
   );
 }
