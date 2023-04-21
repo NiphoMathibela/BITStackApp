04c97dae909bf188c0dac47de5dfaaf789c117e0
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2023-04-21 20:10:34 +0200

    Block component

--- screens/Recipes.js
+++ screens/Recipes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import DisplayBlock from '../components/DisplayBlock'
 
 
 export default function Recipes() {
     return (
-        <View>
+        <View className= "mt-10">
             <Text>Recipes</Text>
+            <DisplayBlock heading= {"Test"} description= {"Big test"}/>
         </View>
     )
 }
