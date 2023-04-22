7745d22f89aeca0f83b23e1f1965f9d73d5ddca1
Pule-Prince <pulemabote01@gmail.com>
2023-04-22 08:06:04 +0200

    Added the camera button, working on the functionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,47 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function Home() {
+const Home = () => {
+
   return (
-    <View>
-        <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.button}>
+        <Pressable  style={styles.button} >
+          <View style={styles.iconContainer}>
+            <Ionicons name={"camera-outline"} size={42} color={"white"}  />
+          </View>
+          <Text style={styles.text}>Snap your Food Item</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#37BD6B',
+    padding: 7,
+    borderRadius: 8,
+  },
+  iconContainer: {
+    backgroundColor: '#37BD6B',
+    borderRadius: 50,
+    padding: 7,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 16,
+  }
+})
+export default Home;
